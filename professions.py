b6686ef60d6851a2d21b89432d81b4b8995113cf
DawidKahla <51680032+DawidKahla@users.noreply.github.com>
2023-02-01 13:53:37 +0000

    add professions file

---
+++ professions.py
@@ -0,0 +1,42 @@
+professions = {
+  'Akolita' : {
+    'atrributes_main' : {
+      'WW': 5,
+      'Us': 5,
+      'Odp': 5,
+      'Int': 10,
+      'SW': 10,
+      'Ogd': 10
+    },
+    'atrributes_sec' : {
+      'Żyw' : 2
+    },
+    'skills' : [
+      'Czytanie i pisanie',
+      'Leczenie',
+      'Nauka (teologia)',
+      'Przekonywanie',
+      'Spostrzegawczość',
+      'Znajomość języka (klasyczny)',
+      'Znajomość języka (staroświatowy)',
+    ],
+    'optional_skills' : [ ['Nauka (astronomia)', 'Nauka (historia)'] ],
+    'abbilities': [ 'Przemawianie' ],
+    'optional_abilities': [
+      ['Bardzo silny', 'Szybki refleks'],
+      ['Charyzmatyczny', 'Urodzony wojownik']
+    ],
+    'trappings' : [
+      'Szaty kapłańskie',
+      'Symbol boga' 
+    ]
+    'next' : [
+      'Cyrulik',
+      'Demagog',
+      'Fanatyk',
+      'Kapłan',
+      'Skryba',
+      'Zakonnik'
+    ]
+  }
+}
