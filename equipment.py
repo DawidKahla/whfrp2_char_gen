db8f3e0e696bfa23fad32b335360fd30d20279ec
DawidKahla <51680032+DawidKahla@users.noreply.github.com>
2023-07-09 22:31:15 +0200

    Code cleanup

--- equipment.py
+++ equipment.py
@@ -1,5 +1,35 @@
+"""
+This module provides Equipment class.
+"""
+from random import randint
+from myrandom import d10, random_choose
+import constants
+
 class Equipment:
+    """
+    Represents the classified equipment of the character.
+
+    Atrributes:
+        trappings (list): The trappings of the character.
+            Format: [trapping1, trapping2, ...].
+        money (int): The money owned by the character in golden corons.
+        basic_armor (int):
+            Character armor points for basic rules.
+        advanced_armor (dict):
+            Character armor points for advanced rules.
+            The keys represent body parts (head, body, arms, legs),
+            and the values represent the armor values for each body part.
+        armor_list (list):
+            List that contains character armor.
+        weapon_list (list):
+            List that contains character weapons.
+
+    """
+    
     def __init__(self) -> None:
+        """
+        Initializes a new instance of the Equipment class.
+        """
         self.trappings = []
         self.money = None
         self.basic_armor = None
@@ -11,3 +41,135 @@ class Equipment:
         }
         self.armor_list = []
         self.weapon_list = []
+
+    def roll_money(self):
+        """
+        Roll the character's starting money.
+
+        Generates the starting money for the character
+        based on random dice rolls.
+        The money is stored in the 'money' attribute of the character.
+        """
+        self.money = d10() + d10()
+
+    def roll_in_trappings(self):
+        """
+        Roll random values in trappings.
+
+        The method iterates over the trappings list and replaces
+        specific patterns with rolled values. The patterns include
+        "1k10/2", "2k10", "3k10", and "6k10" representing dice rolls.
+        The rolled values are inserted into the trappings list.
+
+        """
+        for idx, trapping in enumerate(
+            self.trappings
+        ):
+            self.trappings[idx] = trapping.replace(
+                "1k10/2", f"{randint(1, 5)}")
+            self.trappings[idx] = self.trappings[idx].replace(
+                "2k10", f"{d10()+d10()}"
+            )
+            self.trappings[idx] = self.trappings[idx].replace(
+                "3k10", f"{d10()+d10()+d10()}"
+            )
+            self.trappings[idx] = self.trappings[idx].replace(
+                "6k10", f"{d10() + d10() + d10() + d10() + d10() + d10()}",
+            )
+            self.trappings[idx] = self.trappings[idx].replace(
+                "1k10", f"{d10()}"
+            )
+
+    def take_money_from_trappings(self):
+        """
+        Extract money from trappings.
+
+        The method checks the trappings list for entries
+        containing the string " zk" (e.g., "10 zk"). If a matching
+        entry is found, the corresponding amount is removed from
+        the trappings list and added to the character's money.
+        Cap at "999 zk".
+
+        """
+        for trapping in self.trappings:
+            if " zk" in trapping and len(trapping) < 7:
+                additional_money = int(trapping.replace(" zk", ""))
+                self.trappings.remove(trapping)
+                self.money += additional_money
+
+    def take_armor_from_trappings(self):
+        """
+        Extracts armor from trappings and assigns it.
+
+        The function iterates over the trappings list of the character.
+        If a trapping contains the keyword 'pancerz', it checks for the
+        type of armor and assigns it accordingly.
+        The function also updates the character's advanced_armor dictionary
+        based on the armor details.
+
+        Raises:
+            ValueError: If an unrecognized type of armor is found
+                in the trappings.
+
+        """
+        for trapping in self.trappings:
+            if "pancerz" in trapping:
+                if "lekki pancerz" in trapping:
+                    self.basic_armor = 1
+                elif "średni pancerz" in trapping:
+                    self.basic_armor = 3
+                else:
+                    raise ValueError("Unrecognized type of armor: {trapping}.")
+                for armor_name, armor_detail in constants.armors.items():
+                    if armor_name in trapping:
+                        if "głowa" in armor_detail[0]:
+                            self.advanced_armor["head"] += armor_detail[1]
+                        if "korpus" in armor_detail[0]:
+                            self.advanced_armor["body"] += armor_detail[1]
+                        if "nogi" in armor_detail[0]:
+                            self.advanced_armor["legs"] += armor_detail[1]
+                        if "ręce" in armor_detail[0]:
+                            self.advanced_armor["arms"] += armor_detail[1]
+                        self.armor_list.append(armor_name)
+                self.trappings.remove(trapping)
+
+    def take_weapon_from_trappings(self):
+        """
+        Extracts weapons from trappings and assigns it.
+
+        The function iterates over the trappings list of the character.
+        If trapping contain weapon it's removed from trappings
+        and asigned to weapon_list.
+        """
+        trappings_to_remove = []
+        for trapping in self.trappings:
+            for weapon_name, weapon_detail in constants.weapons.items():
+                if weapon_name in trapping:
+                    if weapon_name == "łuk":
+                        if (
+                            "długi łuk" in trapping
+                            or "elfi łuk" in trapping
+                            or "kislevski łuk konny"
+                        ):
+                            continue
+                    if weapon_name == "topór":
+                        if "dwuręczny topór" in trapping:
+                            continue
+                    if weapon_name == "broń jednoręczna":
+                        self.weapon_list.append(
+                            random_choose(constants.onehand_random_weapon)
+                        )
+                    else:
+                        self.weapon_list.append(weapon_name)
+                    trappings_to_remove.append(trapping)
+                    if "strzał" in trapping:
+                        if weapon_detail[1] == "palna":
+                            self.trappings.append(
+                                "proch i amunicja na 10 strzałów"
+                                )
+                        else:
+                            self.trappings.append("10 strzał")
+                    if "bełt" in trapping:
+                        self.trappings.append("10 bełtów")
+        for trapping in trappings_to_remove:
+            self.trappings.remove(trapping)
--- informations.py
+++ informations.py
@@ -1,8 +1,208 @@
+"""
+This module provides Informations class.
+"""
+from random import randint
+from myrandom import d10, random_choose, mapping_roll
+import constants
+
 class Informations:
+    """
+    Includes a set of additional information on the character.
+
+    Atrributes:
+        race (str): The race of the character.
+        profession (str): The profession of the character.
+        name (str): The name of the character.
+        age (int): The age of the character.
+        siblings (int): The number of siblings the character has.
+        birthplace (str): The birthplace of the character.
+        star (str): The star sign of the character.
+
+    """
+    
     def __init__(self) -> None:
+        """
+        Initializes a new instance of the Informations class.
+        """
+        self.race = None
         self.profession = None
         self.name = None
         self.age = None
         self.star = None
         self.siblings = None
         self.birthplace = None
+
+    def roll_race(self):
+        """
+        Rolls the race for the character.
+
+        Randomly determines character race
+        which can be human, elf, dwarf or halfling.
+        Roll chances are set arbitrarily.
+        """
+        roll = randint(1, 100)
+        if roll == 1:
+            output = "elf"
+        elif roll < 5:
+            output = "dwarf"
+        elif roll < 11:
+            output = "halfling"
+        else:
+            output = "human"
+        self.race = output
+
+    def roll_profession(self):
+        """
+        Rolls the profession for the character based on the character's race.
+
+        Raises:
+            ValueError: If the race is invalid.
+
+        Returns:
+            None
+
+        """
+        if self.race == "elf":
+            mapping = constants.elf_profession_mapping
+        elif self.race == "dwarf":
+            mapping = constants.dwarf_profession_mapping
+        elif self.race == "halfling":
+            mapping = constants.halfling_profession_mapping
+        elif self.race == "human":
+            mapping = constants.human_profession_mapping
+        else:
+            raise ValueError(f"Wrong race in roll_profession {self.race}")
+
+        roll = randint(1, 1000)
+        self.profession = mapping_roll(roll, mapping)
+
+    def roll_name(self):
+        """
+        Roll the character's name based on race and sex.
+
+        Generates the character's name based on random selections
+        from predefined name lists specific to their race and sex.
+        The generated name is stored in the 'name' attribute of the character.
+        """
+        if self.race == "human":
+            surname = random_choose(constants.human_surname_list)
+            if self.sex == "male":
+                firstname = random_choose(constants.human_male_name_list)
+            else:
+                firstname = random_choose(constants.human_female_name_list)
+        if self.race == "dwarf":
+            if self.sex == "male":
+                firstname = f"{random_choose(constants.dwarf_name1_list)}" \
+                    f"{random_choose(constants.dwarf_male_name2_list)}"
+                surname = f"{random_choose(constants.dwarf_name1_list)}" \
+                    f"{random_choose(constants.dwarf_male_name2_list)}son"
+            else:
+                firstname = f"{random_choose(constants.dwarf_name1_list)}" \
+                    f"{random_choose(constants.dwarf_female_name2_list)}"
+                surname = f"{random_choose(constants.dwarf_name1_list)}" \
+                    f"{random_choose(constants.dwarf_female_name2_list)}sdotr"
+        if self.race == "elf":
+            surname = random_choose(constants.elf_surname_list)
+            if d10() < 6:
+                connector = random_choose(constants.elf_name_connector_list)
+            else:
+                connector = ""
+            if self.sex == "male":
+                firstname = f"{random_choose(constants.elf_name1_list)}" \
+                    f"{connector}" \
+                    f"{random_choose(constants.elf_male_name2_list)}"
+            else:
+                firstname = f"{random_choose(constants.elf_name1_list)}" \
+                    f"{connector}" \
+                    f"{random_choose(constants.elf_female_name2_list)}"
+        if self.race == "halfling":
+            surname = random_choose(constants.halfling_surname_list)
+            if self.sex == "male":
+                firstname = f"{random_choose(constants.halfling_name1_list)}" \
+                    f"{random_choose(constants.halfling_male_name2_list)}"
+            else:
+                firstname = f"{random_choose(constants.halfling_name1_list)}" \
+                    f"{random_choose(constants.halfling_female_name2_list)}"
+
+        self.name = f"{firstname} {surname}"
+
+    def roll_age(self):
+        """
+        Roll the character's age based on race.
+
+        Calculates the character's age based on a random dice roll
+        and adjustments specific to their race.
+        The age value is stored in the 'age' attribute of the character.
+        """
+        roll = randint(1, 100)
+        additional_weight = (roll - 1) // 5
+        if self.race == "human":
+            self.age = 16 + additional_weight
+        elif self.race == "halfling":
+            self.age = 20 + 2 * additional_weight
+            if roll > 70:
+                self.age += 2
+        else:
+            self.age = 20 + 5 * additional_weight  # dwarf
+            if self.race == "elf":
+                self.age += 10
+
+    def roll_star(self):
+        """
+        Roll the character's star sign.
+
+        Generates the character's star sign based on
+        a random dice roll and a predefined mapping.
+        The star sign is stored in the 'star' attribute of the character.
+        """
+        roll = randint(1, 100)
+        self.star = mapping_roll(roll, constants.star_mapping)
+
+    def roll_siblings(self):
+        """
+        Roll the number of character's siblings based on race.
+
+        Generates the number of siblings for the character based on
+        a random dice roll and adjustments specific to their race.
+        The number of siblings is stored in the 'siblings' attribute
+        of the character.
+        """
+        roll = d10()
+        if self.race == "dwarf":
+            mapping = constants.siblings_dwarf_mapping
+        elif self.race == "elf":
+            mapping = constants.siblings_elf_mapping
+        else:
+            mapping = constants.siblings_human_mapping
+        self.siblings = mapping_roll(roll, mapping)
+        if self.race == "halfling":
+            self.siblings += 1
+    
+    def roll_birthplace(self):
+        """
+        Roll the character's birthplace based on race.
+
+        Generates the character's birthplace based on random dice rolls
+        and adjustments specific to their race.
+        The birthplace is stored in the 'birthplace' attribute
+        of the character.
+        """
+        if self.race == "dwarf":
+            if randint(1, 100) > 30:
+                self.birthplace = random_choose(
+                    constants.dwarf_birthplace_list
+                )
+        elif self.race == "elf":
+            self.birthplace = mapping_roll(
+                randint(1, 100), constants.elf_birthplace_dict
+            )
+        elif self.race == "halfling":
+            if randint(1, 100) < 50:
+                self.birthplace = constants.halfling_most_common_birthplace
+        if self.birthplace is None:
+            province = random_choose(list(constants.town_dict.keys()))
+            town = mapping_roll(
+                randint(1, 100),
+                constants.town_dict[province]
+            )
+            self.birthplace = f"{province}, {town}"
